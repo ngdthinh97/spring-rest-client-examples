de6d03efdf66c33ebe4a9e6c791260ad2fc75790
ngdthinh97 <ngdthinh.97@gmail.com>
2020-12-27 13:20:50 +0700

    Overload getUsers method

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,10 +2,14 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
@@ -33,4 +37,18 @@ public class ApiServiceImpl implements ApiService {
 		System.out.println("");
 		return userData.getData();
 	}
+
+	@Override
+	public Flux<User> getUsers(Mono<Integer> limit) {
+		// TODO Auto-generated method stub
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
+	}
+	
 }
